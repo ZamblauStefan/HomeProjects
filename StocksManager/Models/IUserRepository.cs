2a009089c3ce63af895a1ac995faa0ff9f474e6c
Zamblau Stefan <zamblau_stefan_2007@yahoo.com>
2023-10-17 13:53:36 +0300

    error on reference //

---
+++ StocksManager/Models/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StocksManager.Models
+{
+    public interface IUserRepository
+    {
+        bool AuthenthicateUser(NetworkCredential credential);
+        void Add(UserModel userModel);
+        void Edit(UserModel userModel);
+        void Remove(int id);
+        UserModel GetById(int id);
+        UserModel GetByUsername(string username);
+        IEnumerable<UserModel> GetBy(string username);
+    }
+}
---
+++ StocksManager/ViewModels/LoginViewModel.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace StocksManager.ViewModels
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        private string _username;
+        private SecureString _password;
+        private string _errorMessage;
+        private bool _isVisible = true;
+
+        //Properties
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                this._username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+
+        public SecureString Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                this._password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+        public string ErrorMessage
+        {
+            get
+            {
+                return this._errorMessage;
+            }
+            set
+            {
+                this.ErrorMessage = value;
+                OnPropertyChanged(nameof(ErrorMessage));
+            }
+        }
+
+        public bool IsVisible
+        {
+            get
+            {
+                return _isVisible;
+            }
+            set
+            {
+                this._isVisible = value;
+                OnPropertyChanged(nameof(IsVisible));
+            }
+        }
+        //-> Commands
+        public ICommand LogInCommand { get; }
+        public ICommand RecoverPasswordCommand { get; }
+        public ICommand ShowPasswordCommand { get; }
+        public ICommand RememberPasswordCommand { get; }
+
+        //Constructor
+        public LoginViewModel()
+        {
+            LogInCommand = new ViewModelCommand(ExecuteLoginCommand, CanExecuteLoginCommand);
+            RecoverPasswordCommand = new ViewModelCommand(p =>ExecuteRecoverPasswordCommand("",""));
+        }
+
+        private void ExecuteRecoverPasswordCommand(string username, string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool CanExecuteLoginCommand(object obj)
+        {
+            bool ValidateData;
+            if(string.IsNullOrWhiteSpace(Username) || Username.Length < 3 || Password == null || Password.Length < 3)
+            {
+                ValidateData = false;
+            }
+            else
+            {
+                ValidateData = true;
+            }
+            return ValidateData;
+        }
+
+        private void ExecuteLoginCommand(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
