0bf627d4a029a125cea1198cdc14dc1b6a9a531c
Zamblau Stefan <zamblau_stefan_2007@yahoo.com>
2024-02-06 10:50:58 +0200

    Create MAUI app

---
+++ MyMAUIapp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MyMAUIapp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MyMAUIapp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MyMAUIapp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
